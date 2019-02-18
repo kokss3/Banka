46eec72e32c9ebd1c348c75bd80211ece52881bd
Koks <kokss324@gmail.com>
2019-02-18 01:22:57 +0100

    Složen Spring Security I obrađuje podatke isključivo preko JDBCa

--- src/main/java/koks/demo/Service/UserService.java
+++ src/main/java/koks/demo/Service/UserService.java
@@ -26,16 +26,18 @@ public class UserService {
 
     //removes multiple entries, and saves new entry, or updates old
     public void saveInDB(User user){
-        String query = "select * from user where name=? and iban=?";
 
+        String query = "select * from user where name=? and iban=?";
         String saving;
 
+        //get current user in list(if there are multiple entries)
         List<User> temp = template.query(query, new Object[]{user.getName(),user.getIban()},(rs, rowNum) ->
                 new User(rs.getInt("id"),rs.getString("name"),
                         rs.getString("iban"),rs.getInt("funds"))
         );
 
         //check if there are multiple entries, already existing or it is non-existent
+            //if there are multiple entries
         if(temp.size()>1) {
             user = fixMultipleEntries(temp,user);
             for (int i = 1; i<temp.size();i++) {
@@ -45,17 +47,20 @@ public class UserService {
             saving = "update user set funds="+user.getFunds()+
                     " where name=\'"+user.getName()+"\' and iban=\'"+user.getIban()+"\'";
 
+            //if there is only one
         }else if(temp.size()!=0){
             user.setFunds(temp.get(0).getFunds()+user.getFunds());
 
             saving = "update user set funds="+user.getFunds()+
                     " where name=\'"+user.getName()+"\' and iban=\'"+user.getIban()+"\'";
 
+            //if there are no entries
         }else {
             saving = "insert into user (iban,funds,name) values(\'"
                     +user.getIban()+"\',"+user.getFunds()+",\'"+user.getName()+"\')";
         }
 
+            //execute query that is generated based on number of entries
         template.execute(saving);
     }
 
